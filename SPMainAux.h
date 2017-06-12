7e32ba6ce419e5e28e5d6f48648c456941edc236
mayacahana <mayacahana@172.16.0.170>
2017-06-12 16:15:30 +0300

    Documentation

--- SPMainAux.h
+++ SPMainAux.h
@@ -4,19 +4,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//put auxiliary functions and constants used by the main function here.
 #define QUIT 0
 #define RESTART 1
 #define ADD_DISC 2
 #define NO_CHANGED 3
 #define UNDO_MOVE 4
-
+/**
+ * initalizing a new game. This function asks for the difficulty level and create a new
+ * gameboard.
+ * @return
+ * the difficulty level of the game
+ */
 int initGame();
+/**
+ * the function is getting an input from the user and parser the command.
+ *
+ * @return
+ * a number that indicates which command should the program commit.
+ *
+ */
 int playturn(SPFiarGame* game, int difficulty, bool winflag);
+/**
+ * Adding a disc to the specified column. Checks if a disk can be put in this column.
+ * If its not possible- an error message appeared.
+ *
+ * @param game- The source game
+ * @param spCmd - the command to parser
+ * @return
+ *
+ *
+ */
 int playAddDisc(SPFiarGame* game, SPCommand spCmd);
+/**
+ * Play the computer's move. Call the suggestMove function and
+ * add the disc to the column number that returned.
+ * Print a message says which move the computer committed.
+ *
+ * @param game - The source game
+ * @param difficulty - the difficulty level of the game
+ *
+ */
 void compPlay(SPFiarGame* game, int difficulty);
+/**
+ * exiting the game. Destroy all the memory. Print a message says the game
+ * is exiting.
+ *
+ * @param game - The source game
+ *
+ */
 void playQuit(SPFiarGame* game);
+/**
+ * Undo the last move - removes a disc that was put in the previous move and changes the current
+ * player's turn. If the user invoked this command more than historySize times
+ * in a row then an error occurs.
+ *
+ * @param game - The source game
+ *
+ */
 int playUndoMove(SPFiarGame* game);
+/**
+ * Play the suggested move offered by the minimax algorithm and print a
+ * message.
+ * @param difficulty - the difficulty level of the game
+ * @param winflag
+ * @param game - The source game
+ *
+ * @return
+ *
+ */
 int playSuggestedMove(int difficulty, bool winflag, SPFiarGame* game);
 SPCommand parseCommand(SPFiarGame* src);
 void winnerDeclaration(char winner);
